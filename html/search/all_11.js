210755ee9c2efb0496ad3b0b3422857ac66cc59d
Benjamin Lueben <jlueben@outlook.com>
2021-11-08 20:20:03 +0100

    added readme.txt

--- html/search/all_11.js
+++ html/search/all_11.js
@@ -6,7 +6,7 @@ var searchData=
   ['should_5fcontinue_176',['should_continue',['../struct_game_state.html#a875b78d9e7325c498ae92f7876308a42',1,'GameState']]],
   ['should_5fexit_177',['should_exit',['../struct_game_state.html#abed68eb30152281479ca85f660b134a5',1,'GameState']]],
   ['singlefile_178',['singleFile',['../structtest_output.html#a427ad18372a271a60a677a428f5faab5',1,'testOutput']]],
-  ['startwebcammonitoring_179',['startWebcamMonitoring',['../main_8cpp.html#a47bd9fe0d8855f97ceb45b7f22719135',1,'main.cpp']]],
+  ['startwebcammonitoring_179',['startWebcamMonitoring',['../main_8cpp.html#acfd947fb78c28874e3700994f0ba6d98',1,'main.cpp']]],
   ['strength_180',['strength',['../struct_player_stats.html#a8f9a1c4507d151edbd39a9f5fdbd9a72',1,'PlayerStats']]],
   ['strengthboni_181',['strengthBoni',['../class_munchkin_card.html#a0bb39e3a28cc9a3c80034e1f8b089b90',1,'MunchkinCard']]]
 ];
